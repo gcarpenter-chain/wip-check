2e9c23daf16435ff97b4fadb3a91d46c0ea4ba54
Gabriel Carpenter <gabriel.carpenter@chainalysis.com>
2020-03-10 16:42:02 +0000

    updates

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,27 +1,14 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
+import {getRegExp} from '../src/main'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execSync(`node ${ip}`, options).toString())
+test.each([
+  ['wip', true],
+  ['Do Not Merge', true],
+  ['wips', false],
+  ['Dontmerge', false]
+])("defaults match '%s': %o", (input, match) => {
+  expect(getRegExp('wip,do not merge').test(<string>input)).toBe(match)
+  // expect(reg.test('wip')).toBe()
+  // expect(reg.test('wips')).toBeFalsy()
+  // expect(reg.test('Do Not Merge')).toBeTruthy()
+  // expect(reg.test('donotmerge')).toBeFalsy()
 })
